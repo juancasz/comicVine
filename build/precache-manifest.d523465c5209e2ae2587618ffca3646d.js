063d9abef354ea0c0e9130416dfebd53553405b2
Juan Jose <juancasz0208@gmail.com>
2020-09-06 23:12:52 -0500

    yarn removed

---
+++ build/precache-manifest.d523465c5209e2ae2587618ffca3646d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1577846cac42e1ba117d652a8a4cc51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58c5e4223e4057e0da0f",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "a9ee111408dda25262c1",
+    "url": "/static/css/main.7caa3d29.chunk.css"
+  },
+  {
+    "revision": "58c5e4223e4057e0da0f",
+    "url": "/static/js/2.4e9846b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4e9846b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9ee111408dda25262c1",
+    "url": "/static/js/main.f156bbf5.chunk.js"
+  },
+  {
+    "revision": "362a62732c655711ddb9",
+    "url": "/static/js/runtime-main.3107d9d6.js"
+  }
+]);
